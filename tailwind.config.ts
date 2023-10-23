c5d5bdcca08423679b0bf3f6cd7965503e35d5d9
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-10-23 16:30:51 -0300

    progress

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,21 @@ const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontFamily: {
+      montserrat: ['var(--font-montserrat)']
     },
+    extend: {
+      colors: {
+        bgblue: '#0B1044',
+        cartPink: '#D41062',
+        buttonSoftPink: '#DF9BB7',
+        productCardBg: '#D9D9D9'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
 export default config
