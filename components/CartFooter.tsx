3b46c8e95a7e55cf38bdd335c01cde7daa6bda5b
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-10-25 17:28:03 -0300

    footer conditional

--- components/CartFooter.tsx
+++ components/CartFooter.tsx
@@ -8,17 +8,36 @@ export default function CartFooter (): JSX.Element {
   const { cartItems, cartSubtotal, cartQuantity } = useShoppingCart()
 
   return (
-    <footer className="bg-cartPink fixed bottom-0 w-full flex items-center justify-between px-6 py-3">
-      <div className='flex flex-col gap-2'>
-        <p className="text-white font-semibold text-lg">Items: <span className='font-light'>{cartQuantity}</span></p>
-        <p className="text-white font-semibold text-lg">Subotal: <span className='font-light'>{cartSubtotal}</span></p>
+    <footer>
+      {cartQuantity > 0 ? 
+      <div className="bg-cartPink fixed bottom-0 w-full flex items-center justify-between px-6 py-3">
+        <div className='flex flex-col gap-2'>
+          <p className="text-white font-semibold text-lg">N° de productos: <span className='font-light'>{cartQuantity}</span></p>
+          <p className="text-white font-semibold text-lg">Subtotal: <span className='font-light'>{cartSubtotal}</span></p>
+        </div>
+        <button
+          onClick={() => {console.log(cartQuantity, cartItems)}} 
+          className="bg-buttonSoftPink text-md text-bgblue font-semibold rounded-3xl px-2 py-2 flex items-center justify-between gap-2 hover:scale-105 ease-out duration-300 hover:shadow-2xl">
+          <FaCartShopping className="w-6 h-4" />
+          Ir al Carrito
+        </button>
       </div>
-      <button
-        onClick={() => {console.log(cartQuantity, cartItems)}} 
-        className="bg-buttonSoftPink text-md text-bgblue font-semibold rounded-3xl px-2 py-2 flex items-center justify-between gap-2 hover:scale-105 ease-out duration-300 hover:shadow-2xl">
-        <FaCartShopping className="w-6 h-4" />
-        Ir al Carrito
-      </button>
+      :
+      ''
+      }
     </footer>
+    
+    // <footer className="bg-cartPink fixed bottom-0 w-full flex items-center justify-between px-6 py-3">
+    //   <div className='flex flex-col gap-2'>
+    //     <p className="text-white font-semibold text-lg">N° de productos: <span className='font-light'>{cartQuantity}</span></p>
+    //     <p className="text-white font-semibold text-lg">Subtotal: <span className='font-light'>{cartSubtotal}</span></p>
+    //   </div>
+    //   <button
+    //     onClick={() => {console.log(cartQuantity, cartItems)}} 
+    //     className="bg-buttonSoftPink text-md text-bgblue font-semibold rounded-3xl px-2 py-2 flex items-center justify-between gap-2 hover:scale-105 ease-out duration-300 hover:shadow-2xl">
+    //     <FaCartShopping className="w-6 h-4" />
+    //     Ir al Carrito
+    //   </button>
+    // </footer>
   )
 }
