070c5f5a90636872e67b8ab582e3a4bf168ba50b
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-10-26 12:54:39 -0300

    mb-40 cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -7,7 +7,7 @@ import Carrito from "@/components/Carrito"
 export default function Cart () {
   const { cartItems, cartSubtotal, cartQuantity } = useShoppingCart()
   return (
-    <div className="mb-20">
+    <div className="mb-40">
       <Carrito />
       {cartQuantity > 0 && <CartFooter />}
     </div>
