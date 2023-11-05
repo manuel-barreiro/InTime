27bae390c3d8cb7c12e72b5e0f63040e3fc8430b
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-11-04 23:57:34 -0300

    Remove unused ESLint rules and update dependencies.

--- app/api/getOrders/route.ts
+++ app/api/getOrders/route.ts
@@ -5,15 +5,14 @@ import { NextResponse } from "next/server";
 
 export async function GET (request: Request) {
 
-  console.log('getOrders')
-
   try {
     await connectMongoDB();
-    const orders = await Order.find().sort( { updatedAt: -1 } );
+    const orders = await Order.find().sort( { updatedAt: -1 } )
+    console.log(orders)
     return NextResponse.json(orders)
 
   } catch(error) {
-    console.log(error)
+    return NextResponse.json(error)
   }
 
 }
